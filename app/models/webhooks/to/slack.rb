060a932170d69b5016b6bc3f25fb80cdeffd05c8
naomichi-y <n.yamakita@gmail.com>
2019-06-09 14:37:39 +0900

    Add SLACK_CHANNEL environment

--- app/models/webhooks/to/slack.rb
+++ app/models/webhooks/to/slack.rb
@@ -1,7 +1,15 @@
 class Webhooks::To::Slack
   def initialize(mention:, url:, additional_message:)
     @mention = "@#{mention}"
-    @channel = @mention == '@everyone' ? '#general' : @mention
+
+    @channel = if ENV.fetch('SLACK_CHANNEL').present?
+      ENV.fetch('SLACK_CHANNEL')
+    elsif @mention == '@everyone'
+      '#general'
+    else
+      @mention
+    end
+
     @text = "#{@mention} #{url} #{additional_message}"
     @webhook_uri = ENV.fetch('SLACK_WEBHOOK_URL')
   end
